ec67f02f7a45ff5fbc283c9fee8e1fa68e0474e5
Daniel <i.daniil2010@yandex.ru>
2021-04-20 22:15:54 +0400

    +

--- QTVersion/Exec/main.cpp
+++ QTVersion/Exec/main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include "../ProxyServer/server.h"
+#include <QtDebug>
 
 
 int main(int argc, char *argv[])
@@ -8,6 +9,7 @@ int main(int argc, char *argv[])
 
     Server server;
     server.listen(QHostAddress::Any,8090);
+    qDebug("Server listening on port 8090");
 
     return a.exec();
 }
